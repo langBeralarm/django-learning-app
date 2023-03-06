bf509f3c5bc48d30b4610581559cc7f0e6d90e5e
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-06 14:06:12 +0100

    Install django-crispy-forms

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "todo.apps.TodoConfig",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap"
