79773889b59c1cd2c59c8f407be3aeb79a90b717
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-06 10:04:58 +0100

    Add todo_list view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+from .models import ToDoItem
+
+
+def todo_list(request):
+    todos = ToDoItem.objects.all()
+    return render(
+        request,
+        "todo/todo_list.html",
+        {
+            "todos": todos,
+        },
+    )
