d48c84d1d44dd8163bc579c1ce7fa932ee9bb06a
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-06 10:03:04 +0100

    Add ToDo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,37 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ToDoItem(models.Model):
+    """
+    ToDoItem is an item to store ToDo's with a title, optional description,
+    creation date and optional due date.
+    """
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True, max_length=1000)
+    creation_date = models.DateTimeField()
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    class ToDoStatus(models.TextChoices):
+        OPEN = "OPN", _("Open")
+        CLOSED = "CLD", _("Closed")
+        IN_PROGRESS = "WIP", _("in Progess")
+
+    status = models.CharField(
+        max_length=3,
+        choices=ToDoStatus.choices,
+        default=ToDoStatus.OPEN,
+    )
+
+    class ToDoPriority(models.TextChoices):
+        LOW = "L", _("Low")
+        NORMAL = "N", _("Normal")
+        HIGH = "H", _("High")
+        URGENT = "U", _("Urgent")
+
+    priority = models.CharField(
+        max_length=1,
+        choices=ToDoPriority.choices,
+        default=ToDoPriority.NORMAL,
+    )
