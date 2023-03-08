9035a1588fd81a16d68c8a22c599f37aa3caf232
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-08 08:42:06 +0100

    Add test for creating todos

--- todo/tests/test_todo_models.py
+++ todo/tests/test_todo_models.py
@@ -7,3 +7,15 @@ class ToDoItemTestCase(TestCase):
     def test_get_todos(self):
         todos = ToDoItem.objects.all().count()
         self.assertEqual(todos, 0)
+
+    def test_create_todo(self):
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 0)
+
+        todo = ToDoItem.objects.create(title="ToDo Item")
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 1)
+
+        self.assertEqual(todo.title, "ToDo Item")
+        self.assertEqual(todo.status, "OPN")
+        self.assertEqual(todo.priority, "N")
