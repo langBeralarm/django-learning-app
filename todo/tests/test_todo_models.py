a564017f760623215250c02825ff56fe3b655c95
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-08 10:16:07 +0100

    Add tests for deleting todos

--- todo/tests/test_todo_models.py
+++ todo/tests/test_todo_models.py
@@ -2,9 +2,14 @@ from django.test import TestCase
 
 from todo.forms import ToDoCreateForm
 from todo.models import ToDoItem
+from todo.views import ToDoDeleteView
 
 
 class ToDoItemTestCase(TestCase):
+    @classmethod
+    def tearDown(cls):
+        ToDoItem.objects.all().delete()
+
     def test_get_todos(self):
         todos = ToDoItem.objects.all().count()
         self.assertEqual(todos, 0)
@@ -40,3 +45,26 @@ class ToDoItemTestCase(TestCase):
 
         todos = ToDoItem.objects.all().count()
         self.assertEqual(todos, 1)
+
+    def test_delete_todo(self):
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 0)
+
+        ToDoItem.objects.create(title="ToDo Item")
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 1)
+
+        ToDoItem.objects.all().delete()
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 0)
+
+    def test_todo_delete_form(self):
+        ToDoItem.objects.create(title="ToDo Item")
+        todos = ToDoItem.objects.all().count()
+        self.assertEqual(todos, 1)
+
+        form_data = {
+            "Confirm": True,
+        }
+        form = ToDoDeleteView.form_class(data=form_data)
+        self.assertTrue(form.is_valid())
