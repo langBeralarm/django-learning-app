88409759d3d06fa25782b9ae5086e1586780fe8d
langBeralarm <leonardo.lang@beralarm.ch>
2023-03-08 09:04:01 +0100

    Add todo_delete view

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ToDoCreateView, ToDoDetailView, ToDoListView
+from .views import ToDoCreateView, ToDoDeleteView, ToDoDetailView, ToDoListView
 
 app_name = "todo"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", ToDoListView.as_view(), name="todo_list"),
     path("create/", ToDoCreateView.as_view(), name="todo_create"),
     path("<int:pk>/detail", ToDoDetailView.as_view(), name="todo_detail"),
+    path("<int:pk>/delete", ToDoDeleteView.as_view(), name="todo_delete"),
 ]
